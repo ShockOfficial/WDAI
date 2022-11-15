1804d1ce3ce2ab034f8d8d2d73735f7704e5c4de
Paweł Motyka <pglmotyka@gmail.com>
2022-11-15 19:05:54 +0100

    tmp commit

--- Lab_3/Zadanie9/script.js
+++ Lab_3/Zadanie9/script.js
@@ -1,13 +1,36 @@
 class Carousel {
 	#items;
 	#currentSlide = 0;
-
 	constructor() {
 		this.#items = [...document.querySelectorAll('.carousel__item')];
 	}
-
 	next() {
-		this;
+		console.log('before');
+		console.log(this.#items);
+
+		const item = this.#items[(this.#currentSlide + 1) % this.#items.length];
+		item.classList = [...item.classList].slice(0, 1) + ['carousel__item--1'];
+
+		this.#items.forEach((item, index) => {
+			const tmp = (index + 1 + this.#currentSlide) % this.#items.length;
+			item.classList.remove(`carousel__item--${tmp}`);
+			item.classList.add(`carousel__item--${tmp + 1}`);
+		});
+		this.#currentSlide++;
+		console.log('after');
+		console.log(this.#items);
 	}
 }
+
+let $nextBtn;
+
+const init = () => {
+	$nextBtn = document.querySelector('.carousel__btn--right');
+	const carousel = new Carousel();
+	$nextBtn.addEventListener('click', () => {
+		carousel.next();
+	});
+};
+
+document.addEventListener('DOMContentLoaded', init);
 
