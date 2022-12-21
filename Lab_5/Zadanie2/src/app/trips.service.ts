6f833ba0a5a6afbb37b5428badcb2ba788afc4f9
Paweł Motyka <pglmotyka@gmail.com>
2022-12-21 10:25:56 +0100

    downgrade angular to 14.2.10

--- Lab_5/Zadanie2/src/app/trips.service.ts
+++ Lab_5/Zadanie2/src/app/trips.service.ts
@@ -7,152 +7,150 @@ import { PriceFilterPipe } from './price-filter.pipe';
 import { RateFilterPipe } from './rate-filter.pipe';
 import { Trip, TripStatus } from './trip/trip.model';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
-import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-	providedIn: 'root',
+  providedIn: 'root',
 })
 export class TripsService {
-	boughtTrips: (Trip & { quantity: number; purchaseDate: number })[] = [];
-	trips: Trip[] = [];
-	incomingTrip = new EventEmitter<true>();
-	constructor(
-		private locationPipe: LocationFilterPipe,
-		private ratePipe: RateFilterPipe,
-		private pricePipe: PriceFilterPipe,
-		private datePipe: DateFilterPipe,
-		private db: AngularFireDatabase,
-	) {
-		this.getTripsFromDb();
-		this.getBoughtTripsFromDb();
-		this.distinctSpecialTrips();
-	}
+  boughtTrips: (Trip & { quantity: number; purchaseDate: number })[] = [];
+  trips: Trip[] = [];
+  incomingTrip = new EventEmitter<true>();
+  constructor(
+    private locationPipe: LocationFilterPipe,
+    private ratePipe: RateFilterPipe,
+    private pricePipe: PriceFilterPipe,
+    private datePipe: DateFilterPipe,
+    private db: AngularFireDatabase
+  ) {
+    this.getTripsFromDb();
+    this.getBoughtTripsFromDb();
+    this.distinctSpecialTrips();
+  }
 
-	getTripsFromDb() {
-		const dbTrips = this.db.list<Trip>('trips').valueChanges();
+  getTripsFromDb() {
+    const dbTrips = this.db.list<Trip>('trips').valueChanges();
 
-		dbTrips.subscribe((data) => {
-			if (this.trips.length === 0) {
-				this.trips = data.map((trip) => ({
-					...trip,
-					startDate: new Date(trip.startDate),
-					endDate: new Date(trip.endDate),
-					currency: trip.currency as Currency, // I know that the data is correct at this point
-					status: TripStatus.Normal,
-				}));
-			}
-			this.distinctSpecialTrips();
-		});
-		return dbTrips;
-	}
+    dbTrips.subscribe((data) => {
+      if (this.trips.length === 0) {
+        this.trips = data.map((trip) => ({
+          ...trip,
+          startDate: new Date(trip.startDate),
+          endDate: new Date(trip.endDate),
+          currency: trip.currency as Currency, // I know that the data is correct at this point
+          status: TripStatus.Normal,
+        }));
+      }
+      this.distinctSpecialTrips();
+    });
+    return dbTrips;
+  }
 
-	async modifyObjectInDb(trip: Trip) {
-		await this.db.list('trips').set(trip.id, {
-			...trip,
-			startDate: trip.startDate.getTime(),
-			endDate: trip.endDate.getTime(),
-		});
-	}
+  async modifyObjectInDb(trip: Trip) {
+    await this.db.list('trips').set(trip.id, {
+      ...trip,
+      startDate: trip.startDate.getTime(),
+      endDate: trip.endDate.getTime(),
+    });
+  }
 
-	async removeFromDb(trip: Trip) {
-		return; // Tymczasowo wyłączone, ale zaimplementowane
+  async removeFromDb(trip: Trip) {
+    return; // Tymczasowo wyłączone, ale zaimplementowane
 
-		await this.db.object(`/trips/${trip.id}`).remove();
-	}
+    await this.db.object(`/trips/${trip.id}`).remove();
+  }
 
-	async saveBoughtTripsInDb() {
-		await this.db.list('bought').set(
-			'list',
-			this.boughtTrips.map((item) => ({
-				...item,
-				startDate: item.startDate.getTime(),
-				endDate: item.endDate.getTime(),
-			})),
-		);
-	}
+  async saveBoughtTripsInDb() {
+    await this.db.list('bought').set(
+      'list',
+      this.boughtTrips.map((item) => ({
+        ...item,
+        startDate: item.startDate.getTime(),
+        endDate: item.endDate.getTime(),
+      }))
+    );
+  }
 
-	getBoughtTripsFromDb() {
-		const trips = this.db
-			.list<Trip & { quantity: number; purchaseDate: number }>('bought/list')
-			.valueChanges();
+  getBoughtTripsFromDb() {
+    const trips = this.db
+      .list<Trip & { quantity: number; purchaseDate: number }>('bought/list')
+      .valueChanges();
 
-		trips.subscribe((data) => {
-			this.boughtTrips = data;
-		});
-		return trips;
-	}
+    trips.subscribe((data) => {
+      this.boughtTrips = data;
+    });
+    return trips;
+  }
 
-	generateId() {
-		return this.db.createPushId();
-	}
+  generateId() {
+    return this.db.createPushId();
+  }
 
-	distinctSpecialTrips() {
-		const filteredTrips = this.getFilteredTrips();
-		filteredTrips.forEach((trip) => (trip.isMostExpensive = 'Normal'));
+  distinctSpecialTrips() {
+    const filteredTrips = this.getFilteredTrips();
+    filteredTrips.forEach((trip) => (trip.isMostExpensive = 'Normal'));
 
-		const availableTrips = filteredTrips.filter(
-			(trip) => trip.currentAmount !== 0,
-		);
+    const availableTrips = filteredTrips.filter(
+      (trip) => trip.currentAmount !== 0
+    );
 
-		if (availableTrips.length === 0) return;
+    if (availableTrips.length === 0) return;
 
-		const mostExpensiveTrip = availableTrips.reduce((prev, current) =>
-			prev.price > current.price ? prev : current,
-		);
-		const cheapestTrip = availableTrips.reduce((prev, current) =>
-			prev.price < current.price ? prev : current,
-		);
-		mostExpensiveTrip.isMostExpensive = 'Expensive';
-		cheapestTrip.isMostExpensive = 'Cheap';
-	}
+    const mostExpensiveTrip = availableTrips.reduce((prev, current) =>
+      prev.price > current.price ? prev : current
+    );
+    const cheapestTrip = availableTrips.reduce((prev, current) =>
+      prev.price < current.price ? prev : current
+    );
+    mostExpensiveTrip.isMostExpensive = 'Expensive';
+    cheapestTrip.isMostExpensive = 'Cheap';
+  }
 
-	removeTrip(trip: Trip) {
-		this.trips = this.trips.filter((tripItem) => tripItem.id !== trip.id);
-		this.distinctSpecialTrips();
-		this.removeFromDb(trip);
-		return this.trips;
-	}
+  removeTrip(trip: Trip) {
+    this.trips = this.trips.filter((tripItem) => tripItem.id !== trip.id);
+    this.distinctSpecialTrips();
+    this.removeFromDb(trip);
+    return this.trips;
+  }
 
-	addTrip(trip: Trip) {
-		this.trips.push(trip);
-		this.distinctSpecialTrips();
-		this.modifyObjectInDb(trip);
-	}
-	getFilteredTrips() {
-		return this.datePipe.transform(
-			this.locationPipe.transform(
-				this.ratePipe.transform(this.pricePipe.transform(this.trips)),
-			),
-		);
-	}
+  addTrip(trip: Trip) {
+    this.trips.push(trip);
+    this.distinctSpecialTrips();
+    this.modifyObjectInDb(trip);
+  }
+  getFilteredTrips() {
+    return this.datePipe.transform(
+      this.locationPipe.transform(
+        this.ratePipe.transform(this.pricePipe.transform(this.trips))
+      )
+    );
+  }
 
-	getById(id: string) {
-		return this.trips.find((trip) => trip.id === id);
-	}
+  getById(id: string) {
+    return this.trips.find((trip) => trip.id === id);
+  }
 
-	buyTrip(trip: Trip & { quantity: number }) {
-		trip.status = TripStatus.Before;
-		this.boughtTrips.push({ ...trip, purchaseDate: Date.now() });
+  buyTrip(trip: Trip & { quantity: number }) {
+    trip.status = TripStatus.Before;
+    this.boughtTrips.push({ ...trip, purchaseDate: Date.now() });
 
-		// Active and incoming trip are activating badge
-		this.boughtTrips.forEach((item) => {
-			if (
-				item.startDate.getTime() >= Date.now() &&
-				this.subtractDays(14, new Date(item.startDate)).getTime() <=
-					Date.now() &&
-				item.endDate.getTime() >= Date.now()
-			) {
-				this.incomingTrip.emit(true);
-			}
-		});
-	}
+    // Active and incoming trip are activating badge
+    this.boughtTrips.forEach((item) => {
+      if (
+        item.startDate.getTime() >= Date.now() &&
+        this.subtractDays(14, new Date(item.startDate)).getTime() <=
+          Date.now() &&
+        item.endDate.getTime() >= Date.now()
+      ) {
+        this.incomingTrip.emit(true);
+      }
+    });
+  }
 
-	subtractDays(numOfDays: number, date = new Date()) {
-		const dateCopy = new Date(date.getTime());
+  subtractDays(numOfDays: number, date = new Date()) {
+    const dateCopy = new Date(date.getTime());
 
-		dateCopy.setDate(dateCopy.getDate() - numOfDays);
+    dateCopy.setDate(dateCopy.getDate() - numOfDays);
 
-		return dateCopy;
-	}
+    return dateCopy;
+  }
 }
-
